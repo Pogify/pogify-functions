2c4bec598980c8fe04f02a2e9c3777674d6396e1
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-08-17 02:12:32 -0400

    host functions written

--- functions/src/ValidateBody.ts
+++ functions/src/ValidateBody.ts
@@ -1,11 +1,43 @@
 export function validateBody(body: { [key: string]: any }) {
-  let valid = [body.timestamp, body.uri, body.position, body.playing].every(
-    Boolean
-  );
+  let errArr = [];
 
-  if (valid) {
-    return Promise.resolve();
-  } else {
-    return Promise.reject("improper body");
+  let { timestamp, uri, position, playing } = body;
+
+  if (!timestamp) {
+    errArr.push("missing timestamp");
+  } else if (typeof timestamp !== "number") {
+    errArr.push("timestamp is not a number");
+  } else if (timestamp < 1597625975267) {
+    errArr.push("timestamp not in milliseconds");
   }
+
+  if (!uri) {
+    errArr.push("missing uri");
+  } else if (typeof uri !== "string") {
+    errArr.push("uri is not string");
+  } else if (!uri.startsWith("spotify:track:")) {
+    errArr.push("improper uri format");
+  }
+
+  if (position === undefined) {
+    errArr.push("missing position");
+  } else if (typeof position !== "number") {
+    errArr.push("position is not a number");
+  }
+
+  if (playing === undefined) {
+    errArr.push("missing playing state");
+  } else if (typeof playing !== "boolean") {
+    errArr.push("playing is not a boolean");
+  }
+
+  if (errArr.length) {
+    throw errArr.join("; ");
+  }
+  return {
+    timestamp,
+    uri,
+    position,
+    playing,
+  };
 }
--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,57 +1,7 @@
-import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
-import "firebase-functions";
-import * as jwt from "jsonwebtoken";
-import { customAlphabet } from "nanoid";
-import { validateBody } from "./ValidateBody";
+// import "firebase-functions";
 
-admin.initializeApp();
+export * from "./HostFunctions";
+
 // // Start writing Firebase Functions
 // // https://firebase.google.com/docs/functions/typescript
 //
-
-const __SECRET = functions.config().jwt.secret;
-
-export const helloWorld = functions.https.onRequest((request, response) => {
-  functions.logger.info("Hello logs!", { structuredData: true });
-  response.send("Hello from Firebase!");
-});
-
-export const startSession = functions.https.onRequest(async (req, res) => {
-  let sessionCode = customAlphabet("abcdefghijklmnopqrstuwxyz0123456789", 5)();
-  let token = jwt.sign(
-    {
-      session: sessionCode,
-    },
-    __SECRET,
-    {
-      expiresIn: "30m",
-    }
-  );
-  try {
-    await validateBody(req.body);
-
-    try {
-      let { timestamp, uri, position, playing } = req.body;
-
-      await admin.database().ref(sessionCode).set({
-        timestamp,
-        uri,
-        position,
-        playing,
-      });
-      res.send({
-        token,
-        session: sessionCode,
-        expiresIn: 30 * 60 * 1000,
-      });
-    } catch (e) {
-      console.error(e);
-    }
-  } catch (reason) {
-    res.status(400).send(reason);
-  }
-});
-export const postUpdate = functions.https.onRequest((req, res) => {});
-export const refreshToken = functions.https.onRequest((req, res) => {});
-export const getInitial = functions.https.onRequest((req, res) => {});
