3fdc62c2b8876d13efe9140df10af7e8a24032c5
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-08-17 21:21:34 -0400

    remove firebase admin as dependency

--- functions/src/HostFunctions.ts
+++ functions/src/HostFunctions.ts
@@ -1,5 +1,4 @@
 import * as functions from "firebase-functions";
-import * as admin from "firebase-admin";
 import axios from "axios";
 import * as jwt from "jsonwebtoken";
 import { customAlphabet } from "nanoid";
