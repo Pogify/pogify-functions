5232f6b12df652302bf7eadb86d4d0db51a887f9
Jeong Yeon Cho <34193668+jeongY-Cho@users.noreply.github.com>
2020-08-18 02:06:25 -0400

    allow preflight checks without auth

--- functions/src/HostFunctions.ts
+++ functions/src/HostFunctions.ts
@@ -105,6 +105,12 @@ export const startSession = functions.https.onRequest(async (req, res) => {
 export const postUpdate = functions.https.onRequest(async (req, res) => {
   // FIXME: proper cors
   res.set("Access-Control-Allow-Origin", "*");
+
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+    return;
+  }
+
   // reject if no authorization
   if (!req.headers.authorization) {
     res.sendStatus(401);
@@ -162,6 +168,13 @@ export const postUpdate = functions.https.onRequest(async (req, res) => {
 export const refreshToken = functions.https.onRequest((req, res) => {
   // FIXME: proper cors
   res.set("Access-Control-Allow-Origin", "*");
+
+  // send 200 on OPTION
+  if (req.method === "OPTIONS") {
+    res.sendStatus(200);
+    return;
+  }
+
   // reject on no authorization
   if (!req.headers.authorization) {
     res.sendStatus(401);
