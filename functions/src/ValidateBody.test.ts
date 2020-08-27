a02d4a4e5666f65f0d6ed0f41a02cf22c925f5c1
jzhengcode <jzhengcode@gmail.com>
2020-08-26 19:19:15 -0700

    added tests for ValidateBody - checks for missing errors and body type errors

---
+++ functions/src/ValidateBody.test.ts
@@ -0,0 +1,39 @@
+import { validateBody } from "./ValidateBody";
+
+describe("testing for body validation", () => {
+  let invalidBody: object = {};
+  let errorMessage: string = "";
+
+  const createErrorMessage = (errorList: string[]) => {
+    return errorList.join("; ");
+  };
+  
+  test("should throw missing errors", () => {
+    errorMessage = createErrorMessage([
+      "missing timestamp",
+      "missing uri",
+      "missing position",
+      "missing playing state"
+    ]);
+    
+    expect(() => { validateBody(invalidBody); }).toThrow(errorMessage);
+  })
+
+  test("should throw type errors", () => {
+    invalidBody = {
+      timestamp: "not a number",
+      uri: 1000,
+      position: "not a number",
+      playing: "not a boolean"
+    };
+
+    errorMessage = createErrorMessage([
+      "timestamp is not a number",
+      "uri is not string",
+      "position is not a number",
+      "playing is not a boolean"
+    ]);
+
+    expect(() => { validateBody(invalidBody); }).toThrow(errorMessage);
+  })
+})
